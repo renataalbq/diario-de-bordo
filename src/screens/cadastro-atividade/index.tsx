a411091f84b98131fc2a1f29e94f830e1303d426
Renata Albuquerque <renataalbq1@gmail.com>
2024-05-03 19:31:00 -0300

    image

--- src/screens/cadastro-atividade/index.tsx
+++ src/screens/cadastro-atividade/index.tsx
@@ -13,7 +13,7 @@ const CadastroAtividade = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     usuario: {
-      id: 0
+      id: 0,
     },
     dataAtividade: "",
     descricaoAtividade: "",
@@ -21,7 +21,7 @@ const CadastroAtividade = () => {
     atividadeTipo: 0,
   });
   const { cadastrarAtividade, error } = useCadastrarAtividade();
-
+  const [base64, setBase64] = useState("");
   const { transcript, listening, browserSupportsSpeechRecognition } =
     useSpeechRecognition();
 
@@ -40,7 +40,7 @@ const CadastroAtividade = () => {
     }
   };
 
-  const handleSave = async (event: { preventDefault: () => void; }) => {
+  const handleSave = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
     await cadastrarAtividade(formData);
     if (!error) {
@@ -48,7 +48,9 @@ const CadastroAtividade = () => {
     }
   };
 
-  const handleInputChange = (e: { target: { name: string; value: string } }) => {
+  const handleInputChange = (e: {
+    target: { name: string; value: string };
+  }) => {
     const { name, value } = e.target;
     if (name === "dataAtividade") {
       setFormData({
@@ -63,12 +65,26 @@ const CadastroAtividade = () => {
     }
   };
 
+  const handleFileChange = (event: { target: { files: unknown[]; }; }) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setBase64(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <>
       <Menu />
       <div className="container mx-auto p-4">
         <h1 className="text-2xl font-bold mb-4">Cadastro de Atividade</h1>
-        <form onSubmit={handleSave} className="bg-gray-50 p-4 rounded-md mx-auto">
+        <form
+          onSubmit={handleSave}
+          className="bg-gray-50 p-4 rounded-md mx-auto"
+        >
           <div className="mb-4">
             <label
               htmlFor="titulo"
@@ -82,7 +98,7 @@ const CadastroAtividade = () => {
                 name="titulo"
                 type="text"
                 autoComplete="titulo"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                className="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                 value={formData.titulo}
                 onChange={handleInputChange}
               />
@@ -102,7 +118,7 @@ const CadastroAtividade = () => {
                 autoComplete="tipo-atividade"
                 value={formData.atividadeTipo}
                 onChange={handleInputChange}
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                className="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
               >
                 <option value="">Selecione um Tipo</option>
                 <option value="0">Atividade de Campo</option>
@@ -123,7 +139,7 @@ const CadastroAtividade = () => {
                 id="dataAtividade"
                 name="dataAtividade"
                 type="date"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                className="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                 value={formData.dataAtividade}
                 onChange={handleInputChange}
               />
@@ -137,30 +153,31 @@ const CadastroAtividade = () => {
               Imagem:
             </label>
             <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-              <div className="text-center">
-                <PhotoIcon
-                  className="mx-auto h-12 w-12 text-gray-300"
-                  aria-hidden="true"
-                />
-                <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                  <label
-                    htmlFor="file-upload"
-                    className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                  >
-                    <span>Escolha a imagem</span>
-                    <input
-                      id="file-upload"
-                      name="file-upload"
-                      type="file"
-                      className="sr-only"
-                    />
-                  </label>
-                  <p className="pl-1">ou arraste e solte</p>
+              {base64 ? (
+                <img src={base64} alt="Preview" />
+              ) : (
+                <div className="text-center">
+                  <PhotoIcon
+                    className="mx-auto h-12 w-12 text-gray-300"
+                    aria-hidden="true"
+                  />
+                  <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                    >
+                      <span>Escolha a imagem</span>
+                      <input
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                        className="sr-only"
+                        onChange={handleFileChange}
+                      />
+                    </label>
+                  </div>
                 </div>
-                <p className="text-xs leading-5 text-gray-600">
-                  PNG, JPG up to 10MB
-                </p>
-              </div>
+              )}
             </div>
           </div>
           <div className="mb-4">
@@ -172,7 +189,7 @@ const CadastroAtividade = () => {
             </label>
             <div className="mt-2">
               <textarea
-                className="border rounded-md p-2 w-full h-32"
+                className="border pl-2 rounded-md p-2 w-full h-32"
                 value={formData.descricaoAtividade || transcript}
                 onChange={(e) => {
                   setFormData({
@@ -192,9 +209,7 @@ const CadastroAtividade = () => {
           </div>
           <button
             className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            type="submit"
-            onClick={handleSave}
-          >
+            type="submit">
             Salvar Atividade
           </button>
         </form>
